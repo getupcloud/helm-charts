52a4f4c08fb23949a19ba00e619d7475ef5259b3
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2021-04-20 19:55:17 -0300

    Fix chart size and bump

--- grafana-extra-dashboards/get-linkerd-dashboards.sh
+++ grafana-extra-dashboards/get-linkerd-dashboards.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-git clone https://github.com/linkerd/linkerd2.git
-(
-  echo Checkout release-2.10
-  cd linkerd2
-  git checkout -b release-2.10 remotes/origin/release/stable-2.10
-)
+if ! [ -d linkerd2 ]; then
+   git clone https://github.com/linkerd/linkerd2.git
+    echo Checkout release-2.10
+    pushd linkerd2
+    git checkout -b release-2.10 remotes/origin/release/stable-2.10
+    popd
+else
+    echo Pulling release-2.10
+    pushd linkerd2
+    git pull
+    popd
+fi
 
 rm -rf dashboards/linkerd
 mkdir -p dashboards/linkerd
 
 for i in linkerd2/grafana/dashboards/*.json; do
-  cp -vf $i dashboards/linkerd/linkerd-${i##*/}
+  sed -e 's,<script.*</script>,,g' $i > dashboards/linkerd/linkerd-${i##*/}
 done
-rm -rf linkerd2
+
+#rm -rf linkerd2
