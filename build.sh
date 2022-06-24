f9f6f4931faa5e7acde0aa9df5480708afe67d28
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-06-24 15:21:33 -0300

    add scheduled-scaler

--- build.sh
+++ build.sh
@@ -30,6 +30,8 @@ echo Downloading git repos
 charts+=( git/postgres-operator-examples/helm/install/ )
 ./git-download.sh https://github.com/rchakode/kube-opex-analytics.git
 charts+=( git/kube-opex-analytics/manifests/helm/ )
+./git-download.sh https://github.com/k8s-restdev/scheduled-scaler.git
+charts+=( git/scheduled-scaler/artifacts/kubes/scaling/chart )
 
 echo Charts sources: ${charts[*]}
 
